d9e15a51f01cafa52397663fd481219273069df8
Ahmed Sherif <ahmedsherif91101@gmail.com>
2024-05-16 13:49:05 +0300

    report 16 5 2024

--- Business/FI/Account/FiAccountService.cs
+++ Business/FI/Account/FiAccountService.cs
@@ -693,9 +693,6 @@ namespace Business.FI.Account
 
                     }
                     break;
-
-
-
                 case "AccountCreditAccountsReport":
                     {
                         List<FiAccountItemBalancesViewModel> CreditAccounts;
@@ -763,7 +760,7 @@ namespace Business.FI.Account
                 case "QualitativeAnalysisListReport":
                     {
                         List<AccountItemVM> QualitativeAnalysis = new List<AccountItemVM>();
-                        QualitativeAnalysis = GetFinancialCenterReportData(fiscalYearId, null, 0);
+                        QualitativeAnalysis = GetFinancialCenterReportData(fiscalYearId, "3", 0);
                         report.DataSources.Add(new ReportDataSource() { Name = "TemporaryAdvances", Value = QualitativeAnalysis });
                     }
                     break;
