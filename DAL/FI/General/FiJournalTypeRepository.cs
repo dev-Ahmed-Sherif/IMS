39bef0538fa395d40dea809de28eec4992d05e22
Mokhtar <mmelnobey92@gmail.com>
2024-05-27 10:16:08 +0300

    add jpurnal Type

---
+++ DAL/FI/General/FiJournalTypeRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Models.FI.Journal;
+using Entities.ViewModels.FI.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.FI.General
+{
+    public class FiJournalTypeRepository
+    {
+        private AppDbContext _context;
+        public FiJournalTypeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public List<FiJournalTypeVM> GetAll()
+           => _context.FiJournalTypes.Select(n => new FiJournalTypeVM
+           {
+               Id = n.Id,
+               JournalType = n.JournalType,
+           }).ToList();
+    }
+}
