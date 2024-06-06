c94e5c9a8f485625184df1bb39a6ffeeb4bed243
Deveoper3 <Deveoper3@gmail.com>
2024-06-06 13:56:42 +0300

    REport HR

--- Business/HR/HrEmployeeService.cs
+++ Business/HR/HrEmployeeService.cs
@@ -89,9 +89,24 @@ namespace Business.HR
 
                 report.DataSources.Add(new ReportDataSource() { Name = "HrEmployee", Value = HrEmployee });
             }
+            else if (reportName == "EmployeeReport")
+            {
+                HrEmployee = Search(searchModel);
+
+                report.DataSources.Add(new ReportDataSource(){ Name = "HrEmployee" ,  Value = HrEmployee});
+            }
+
+            else if(reportName == "EmployeeStatisticsReport")
+            {
+                HrEmployee = Search(searchModel);
+                report.DataSources.Add(new ReportDataSource() { Name = "HrEmployee", Value = HrEmployee});
+            }
+
+
+
             // Dictionary<string, string> parameters = new Dictionary<string, string>();
 
-            // var result = report.Execute(Entities.Helpers.ReportHelper.GetRenderType(reportType), 1, parameters);
+                // var result = report.Execute(Entities.Helpers.ReportHelper.GetRenderType(reportType), 1, parameters);
 
             byte[] renderedBytes = report.Render(reportType);
 
