cef1e0e6da00c8614c2bf2a687d9053d9243ecac
Ahmed Sherif <ahmedsherif91101@gmail.com>
2024-06-09 13:25:16 +0300

    Accounts From App Settings

--- IMS/Controllers/FI/Account/FIAccountController.cs
+++ IMS/Controllers/FI/Account/FIAccountController.cs
@@ -1,7 +1,9 @@
 ﻿using Business.FI.Account;
 using Entities.ReportViewModels;
 using Entities.ViewModels.FI.Account;
+using Entities.Constants.FiConstants;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Net.Mime;
@@ -14,10 +16,12 @@ namespace IMS.Controllers.FI.Account
     public class FIAccountController : ControllerBase
     {
         private FiAccountService _FiAccountService;
+        private readonly FiNewAccountsCodes _fiAccountsCodes;
 
-        public FIAccountController(FiAccountService FiAccountService)
+        public FIAccountController(FiAccountService FiAccountService, IOptionsSnapshot<FiNewAccountsCodes> fiAccountsCodes)
         {
             _FiAccountService = FiAccountService;
+            _fiAccountsCodes = fiAccountsCodes.Value;
         }
 
         [HttpPost("Add")]
@@ -77,7 +81,7 @@ namespace IMS.Controllers.FI.Account
         }
 
         [HttpGet("get/data/by/Code/{code}/{startDate}/{endDate}")]
-        public IActionResult GetAccountMasterReportData(string code,int fiscalYearId)
+        public IActionResult GetAccountMasterReportData(string code, int fiscalYearId)
         {
             var Account = _FiAccountService.GetAccountMasterReportData(code, fiscalYearId);
             return Ok(Account);
@@ -139,6 +143,10 @@ namespace IMS.Controllers.FI.Account
         //{
         //    return Ok(await _FiAccountService.GetFixedAssetsFinancialCenterData(fiscalYearId));
         //}
-
+        [HttpGet("Test")]
+        public IActionResult Test()
+        {
+            return Ok(_fiAccountsCodes);
+        }
     }
 }
