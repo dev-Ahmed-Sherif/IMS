490483eb46c71da845a178ffce0df5c1c51f7d0e
Mokhtar <mmelnobey92@gmail.com>
2024-06-11 17:16:35 +0300

    update_Report

---
+++ Entities/ViewModels/Part1ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ViewModels
+{
+    public class Part1ViewModel
+    {
+        public int Num { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal NetBasedOnDebit { get; set; }
+        public decimal Stale { get; set; }
+        public decimal Net { get; set; }
+    }
+}
